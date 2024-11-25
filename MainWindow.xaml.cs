7830de95787b15b869a56d0bd088985bef67496f
KolbazovFil <mr.kolbazov@yandex.ru>
2024-11-25 16:21:31 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace _16PR_Kolbazov_RPM
+{
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Note> Notes { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            Notes = new ObservableCollection<Note>();
+            NotesListBox.ItemsSource = Notes;
+        }
+        private void AddNoteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(TitleTextBox.Text) || string.IsNullOrWhiteSpace(ContentTextBox.Text))
+            {
+                MessageBox.Show("Пожалуйста, заполните заголовок и текст заметки!");
+                return;
+            }
+            var newNote = new Note { Title = TitleTextBox.Text, Content = ContentTextBox.Text };
+            Notes.Add(newNote);
+            ClearInputFields();
+        }
+        private void EditNoteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (NotesListBox.SelectedItem is Note selectedNote)
+            {
+                if (string.IsNullOrWhiteSpace(TitleTextBox.Text) || string.IsNullOrWhiteSpace(ContentTextBox.Text))
+                {
+                    MessageBox.Show("Пожалуйста, заполните заголовок и текст заметки!");
+                    return;
+                }
+                selectedNote.Title = TitleTextBox.Text;
+                selectedNote.Content = ContentTextBox.Text;
+            }
+        }
+        private void DeleteNoteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (NotesListBox.SelectedItem is Note selectedNote)
+            {
+                Notes.Remove(selectedNote);
+                ClearInputFields();
+            }
+        }
+        public class Note
+        {
+            public string Title { get; set; }
+            public string Content { get; set; }
+            public override string ToString()
+            {
+                return $"{Title} - {Content}";
+            }
+        }
+
+
+        private void NotesListBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if (NotesListBox.SelectedItem is Note selectedNote)
+            {
+                TitleTextBox.Text = selectedNote.Title;
+                ContentTextBox.Text = selectedNote.Content;
+
+                EditNoteButton.IsEnabled = true;
+                DeleteNoteButton.IsEnabled = true;
+            }
+            else
+            {
+                EditNoteButton.IsEnabled = false;
+                DeleteNoteButton.IsEnabled = false;
+            }
+        }
+        private void ClearInputFields()
+        {
+            TitleTextBox.Clear();
+            ContentTextBox.Clear();
+        }
+    }
+}
